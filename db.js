f86f2c0f339284a397c9752a84038801926c4003
luximeng <lucymeng2009@gmail.com>
2022-02-10 14:37:27 -0800

    add everything

---
+++ db.js
@@ -0,0 +1,18 @@
+const destinations = [
+    {
+    destination: "Eiffle Tower",
+    location: "Paris, France"
+    }
+
+]
+
+
+const citis = ["LA","Paris"]
+// export
+// module.exports = destinations; 
+
+// console.log(module);
+module.exports = {
+    destinations, 
+    citis,
+};
---
+++ index.js
@@ -0,0 +1,19 @@
+//import, don't need .js cos node knows it's js
+const db = require("./db")
+const express = require("express")
+
+const PORT = 3000
+
+//creates an express server 
+const server = express()
+server.listen(PORT, ()=>{
+    console.log(`Server listening on PORT: ${PORT}`);
+});
+
+// Endpoints(ROUTES) are made up of the method and the path
+// GET / 
+// curly braces is like creating a key value for new object
+server.get("/", (req, res)=>{
+    res.send({db});
+});
+
